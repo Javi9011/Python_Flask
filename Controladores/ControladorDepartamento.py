84f32053969cfd32027a54ece9daee02df91010d
Javi9011 <javo9011@gmail.com>
2022-10-30 14:52:49 -0500

    Cambios Departamento

--- Controladores/ControladorDepartamento.py
+++ Controladores/ControladorDepartamento.py
@@ -1,3 +1,13 @@
+from Modelos.Departamento import Departamento
+
+
 class ControladorDepartamento():
     def __init__(self):
         print("Entra al constructor de la clase controlador")
+
+    def createDepartamento(self, bodyRequest):
+        print("Creando el departamento...")
+        nuevoDepartamento = Departamento(bodyRequest)
+        print("Departamento a crear en la BD: ", nuevoDepartamento.__dict__)
+        self.repositorioDepartamento.save(nuevoDepartamento)
+        return True
